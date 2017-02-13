9d6576c18549497a3c93d667bb4ce7adb6ae9d4c
Guillaume Blin <guillaume.blin@labri.fr>
2017-02-13 16:02:14 +0100

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -5,7 +5,8 @@ class block_vpl_aria_edit_form extends block_edit_form {
     protected function specific_definition($mform) {
 
         $mform->addElement('advcheckbox', 'config_divvisbile', 'Debug mode','',array(), array(0, 1));
-
+        $mform->addElement('text', 'config_editkeysc', 'Array of keys for edit shortcut as [14,22,188]');
+        $mform->setDefault('config_editkeysc', '');
         $mform->addElement('text', 'config_savekeysc', 'Array of keys for save shortcut as [14,22,188]');
         $mform->setDefault('config_savekeysc', '');
         $mform->addElement('text', 'config_runkeysc', 'Array of keys for run shortcut as [14,22,188]');
@@ -18,6 +19,7 @@ class block_vpl_aria_edit_form extends block_edit_form {
         $mform->setDefault('config_cregex', '');
         $mform->addElement('text', 'config_eregex', 'Regex on execution');
         $mform->setDefault('config_eregex', '');
+        $mform->setType('config_editkeysc', PARAM_RAW);
         $mform->setType('config_savekeysc', PARAM_RAW);
         $mform->setType('config_runkeysc', PARAM_RAW);
         $mform->setType('config_evalkeysc', PARAM_RAW);
